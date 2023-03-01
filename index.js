727b891995938ef7960fac169337eba26bb211e9
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2023-02-28 20:40:36 -0700

    Add body-parser middleware

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 const authRoute = require('./routes/authRoutes');
 const apiRoute = require('./routes/apiRoutes');
@@ -14,6 +15,7 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
 	cookieSession({
 		maxAge: THIRTY_DAYS,
