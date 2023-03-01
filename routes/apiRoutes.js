546efcba8b9365e2d01c113e4a11308543dd81ec
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2023-02-28 20:41:26 -0700

    Use stripe to create charge and add credits to user

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,29 @@
 const express = require('express');
+const keys = require('../config/keys');
+const requireLogin = require('../middlewares/requireLogin');
+const stripe = require('stripe')(keys.stripeSecretKey);
 const router = express.Router();
 
 router.get('/logout', (req, res) => {
 	req.logout();
-	res.send(req.user);
+	res.redirect('/');
 });
 
 router.get('/current_user', (req, res) => {
 	res.send(req.user);
+});
+
+router.post('/stripe', requireLogin, async (req, res) => {
+	const charge = await stripe.charges.create({
+		amount: 500,
+		currency: 'usd',
+		description: '$5 for 5 credits',
+		source: req.body.token.id,
+	});
+
+	req.user.credits += 5;
+	const user = await req.user.save();
+	res.send(user);
 });
 
 module.exports = router;
