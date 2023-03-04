d24dab8d74479fa36f15b30ced90d3293b2d106c
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2023-03-03 18:27:43 -0700

    Setup for deployment

--- index.js
+++ index.js
@@ -29,5 +29,18 @@ app.use(passport.session());
 app.use('/auth', authRoute);
 app.use('/api', apiRoute);
 
+if (process.env.NODE_ENV === 'production') {
+	// Express will serve up production assets
+	// like our main.js or main.css file
+	app.use(express.static('client/build'));
+
+	// Express will serve up the index.html file
+	// if it doesn't recognize route after checking above routes
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
